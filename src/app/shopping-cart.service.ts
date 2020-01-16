d5df61d2ab765128672c4b596704b67f3ff1714c
FurrG <A2674151a>
2020-01-16 19:13:46 +0200

    cart service implementation

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
 
+import { GoodItem } from './goods.service';
+
+interface CartItem {
+  good: GoodItem;
+  quantity: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingCartService {
 
+  cart: CartItem[] = [];
+
   constructor() { }
+
+  private findGoodIndex(goodId: number): number{
+    return this.cart.findIndex(el => el.good.id === goodId);
+  }
+
+  add(good: GoodItem){
+    const goodIdx = this.findGoodIndex(good.id);
+    if (goodIdx === -1){
+      this.cart.push({good, quantity: 1});
+    } else {
+      this.cart[goodIdx].quantity++;
+    }
+  }
+
+  remove(goodId: number) {
+    const goodIdx = this.findGoodIndex(goodId);
+    if (goodIdx !== -1) {
+      if (this.cart[goodIdx].quantity > 1) {
+        this.cart[goodIdx].quantity--;
+      } else {
+        this.cart.splice(goodIdx, 1);
+      }
+    }
+  }
+
+  isInCart(goodId: number): boolean {
+    const goodIdx = this.findGoodIndex(goodId);
+    return goodIdx !== -1;
+  }
+
+  empty(){
+    this.cart = [];
+  }
+
+  orderSum(): number {
+    let sum = 0;
+    for (const cartItem of this.cart) {
+      sum += cartItem.quantity * cartItem.good.price;
+    }
+    return sum;
+  }
+
+
 }
